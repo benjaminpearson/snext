6ef69b2a9f75e48f75ca922687960c8ed25ae077
Benjamin Pearson <benjaminpearson@users.noreply.github.com>
2023-09-14 01:06:29 +1000

    Implement Playwright (#5)
    * chore: install playwright
    
    * fix: eslint conflicting with playwright
    
    * chore: add eslint playwright
    
    * chore: playwright config

---
+++ playwright.config.js
@@ -0,0 +1,37 @@
+import { defineConfig } from '@playwright/test';
+
+export default defineConfig({
+  // Look for test files in this directory, relative to this configuration file.
+  testDir: 'src/e2e',
+
+  // Run all tests in parallel.
+  fullyParallel: true,
+
+  // Fail the build on CI if you accidentally left test.only in the source code.
+  forbidOnly: !!process.env.CI,
+
+  // Retry on CI only.
+  retries: process.env.CI ? 2 : 0,
+
+  // Opt out of parallel tests on CI.
+  workers: process.env.CI ? 1 : undefined,
+
+  // Reporter to use
+  reporter: 'html',
+
+  use: {
+    // Base URL to use in actions like `await page.goto('/')`.
+    baseURL: 'http://localhost:3000',
+
+    // Collect trace when retrying the failed test.
+    trace: 'on-first-retry',
+  },
+  // Run your local dev server before starting the tests.
+  webServer: {
+    command: 'pnpm build && pnpm start',
+    url: 'http://localhost:3000',
+    reuseExistingServer: !process.env.CI,
+  },
+  // Output test results
+  outputDir: './src/e2e/test-results',
+});
