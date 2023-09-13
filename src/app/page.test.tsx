07ef4de65ec82f24405d0639b96e97fa74e87b73
Benjamin Pearson <benjaminpearson@users.noreply.github.com>
2023-09-13 23:43:36 +1000

    Implement Jest (#4)
    * chore: add jest
    
    * chore: use node test environment by default
    
    * chore: add eslint plugins
    
    * chore: check prettier before linting

---
+++ src/app/page.test.tsx
@@ -0,0 +1,23 @@
+/**
+ * @jest-environment jsdom
+ */
+import '@testing-library/jest-dom';
+import { render, screen } from '@testing-library/react';
+
+import Home from './page';
+
+describe('Home', () => {
+  it('renders the Home page - Test 1', () => {
+    render(<Home />);
+    expect(screen.getByText('Get started by editing')).toBeInTheDocument();
+  });
+
+  it('renders the Home page - Test 2', () => {
+    render(<Home />);
+    expect(
+      screen.getByText(
+        'Find in-depth information about Next.js features and API.',
+      ),
+    ).toBeInTheDocument();
+  });
+});
