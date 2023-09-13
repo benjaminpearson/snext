36cc0038bbccbcf56a4d5b519ca4e15b2b3df2f0
Benjamin Pearson <benjaminpearson@users.noreply.github.com>
2023-09-14 00:59:46 +1000

    chore: playwright config

--- src/e2e/example.test.ts
+++ src/e2e/example.test.ts
@@ -1,7 +1,7 @@
 import { test, expect } from '@playwright/test';
 
 test('should navigate to the home page', async ({ page }) => {
-  await page.goto('http://localhost:3000/');
+  await page.goto('/');
   await expect(page).toHaveURL('http://localhost:3000');
   await expect(
     page.getByRole('img', { name: 'Next.js Logo' }),
