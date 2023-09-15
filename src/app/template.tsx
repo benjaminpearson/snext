81eeb3aa95b38cac916f1e86c6bc44d663ef3acd
Benjamin Pearson <benjaminpearson@users.noreply.github.com>
2023-09-15 22:32:04 +1000

    refactor: remove get locale in root layout

--- src/app/template.tsx
+++ src/app/template.tsx
@@ -3,14 +3,8 @@ import { BraidProvider, ContentBlock, Text, apacTheme } from '@/lib/braid';
 import { useCurrentLocale } from 'next-i18n-router/client';
 import i18nConfig from '../../i18n.config';
 
-export default function Template({
-  children,
-  params,
-}: {
-  children: React.ReactNode;
-  params: { locale: string };
-}) {
-  const locale = useCurrentLocale(i18nConfig) ?? params.locale ?? 'en';
+export default function Template({ children }: { children: React.ReactNode }) {
+  const locale = useCurrentLocale(i18nConfig) ?? 'en';
   return (
     <BraidProvider theme={apacTheme}>
       {children}
