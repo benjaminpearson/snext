52466ee5afb9ea27f4f070b270960ce4c4ad53e8
Benjamin Pearson <benjaminpearson@users.noreply.github.com>
2023-09-16 10:02:21 +1000

    feat: vocab provider added

--- src/app/template.tsx
+++ src/app/template.tsx
@@ -1,16 +1,19 @@
 'use client';
 import { BraidProvider, ContentBlock, Text, apacTheme } from '@/lib/braid';
 import { useCurrentLocale } from 'next-i18n-router/client';
+import { VocabProvider } from '@/lib/vocab';
 import i18nConfig from '../../i18n.config';
 
 export default function Template({ children }: { children: React.ReactNode }) {
   const locale = useCurrentLocale(i18nConfig) ?? 'en';
   return (
     <BraidProvider theme={apacTheme}>
-      {children}
-      <ContentBlock>
-        <Text>Locale: {locale}</Text>
-      </ContentBlock>
+      <VocabProvider language={locale}>
+        {children}
+        <ContentBlock>
+          <Text>Locale: {locale}</Text>
+        </ContentBlock>
+      </VocabProvider>
     </BraidProvider>
   );
 }
