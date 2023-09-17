ffd72282edd124d01a9b85343da6a72ab7f98867
Benjamin Pearson <benjaminpearson@users.noreply.github.com>
2023-09-18 00:16:32 +1000

    feat: add braid viewports

---
+++ .storybook/viewports.ts
@@ -0,0 +1,34 @@
+export default {
+  mobile: {
+    name: 'Mobile',
+    styles: {
+      width: '320px',
+      height: '568px',
+    },
+    type: 'mobile',
+  },
+  tablet: {
+    name: 'Tablet',
+    styles: {
+      width: '740px',
+      height: '1024px',
+    },
+    type: 'tablet',
+  },
+  desktop: {
+    name: 'Desktop',
+    styles: {
+      width: '992px',
+      height: '1194px',
+    },
+    type: 'desktop',
+  },
+  wide: {
+    name: 'Wide',
+    styles: {
+      width: '1200px',
+      height: '1920px',
+    },
+    type: 'desktop',
+  },
+};
